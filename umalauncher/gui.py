2d347338a1eb8b1532a76b0cf60d065b9621a71a
qwcan <ieatspamforbreakfasts1@gmail.com>
2025-09-12 08:01:20 -0500

    Update discord link

--- umalauncher/gui.py
+++ umalauncher/gui.py
@@ -1363,7 +1363,7 @@ class UmaErrorPopup(qtw.QMessageBox):
     def __init__(self, parent, title: str, message: str, traceback_str: str, user_id: str, msg_icon: qtw.QMessageBox.Icon = qtw.QMessageBox.Icon.Information):
         super().__init__()
         self.setWindowTitle(title)
-        self.setText(f"<b>{message}<br>You may send this error report to the developer to help fix this issue.<br>If an error appears multiple times, please join the <a href=\"https://discord.gg/wvGHW65C6A\">Discord server</a>, because the developer might need your help to fix the issue.</b><br>{traceback_str}")
+        self.setText(f"<b>{message}<br>You may send this error report to the developer to help fix this issue.<br>If an error appears multiple times, please join the <a href=\"https://discord.gg/xd4EbvKBaf\">Discord server</a>, because the developer might need your help to fix the issue.</b><br>{traceback_str}")
         upload_button = qtw.QPushButton("Send error report")
         upload_button.clicked.connect(lambda: self.upload_error_report(traceback_str, user_id))
         self.addButton(upload_button, qtw.QMessageBox.ButtonRole.ActionRole)
